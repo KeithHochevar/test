4b15817289bea9ce3ed8f2a4c0cf18d8d0c09cd0
KeithHochevar <36213289+KeithHochevar@users.noreply.github.com>
2018-04-03 21:00:42 -0400

    Add files via upload

---
+++ InClass-Fayomi-20180327/InClass-Fayomi-20180327/InClass-Fayomi-20180327/Car.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass_Fayomi_20180327
+{
+    class Car
+    {
+        public string make;
+        public string model;
+        public int year;
+        private int speed = 0;
+        private int topSpeed;
+
+        public void SpeedUp()
+        {
+            // speed = speed + 5;
+            if (speed < topSpeed)
+            {
+                speed++;
+            }
+        }
+
+        public void SlowDown()
+        {
+            if (speed > 0)
+            {
+                speed--;
+            }
+            // Console.WriteLine("Slowing Down...");
+        }
+
+        public void ToString()
+        {
+            Console.WriteLine("I'm a " + make + " " + model + "and I'm going " + speed + "mph");
+        }
+
+        public void setTopSpeed(int pTopSpeed)
+        {
+            this.topSpeed = pTopSpeed;
+        }
+
+        public int getSpeed()
+        {
+            return speed;
+        }
+
+        public void Honk()
+        {
+            Console.WriteLine("Honk Honk!");
+        }
+
+        public void Beep()
+        {
+            Console.WriteLine("BEEP BEEP!");
+        }
+    }
+}
---
+++ InClass-Fayomi-20180327/InClass-Fayomi-20180327/InClass-Fayomi-20180327/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass_Fayomi_20180327
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Car car1 = new Car();
+
+            car1.make = "Chevy";
+            car1.model = "Cruze";
+            //car1.setTopSpeed(110);
+            car1.SpeedUp();
+            car1.SpeedUp();
+            car1.SpeedUp();
+            car1.SpeedUp();
+            car1.SpeedUp();
+            car1.SpeedUp();
+            Console.WriteLine("Current Speed:" + car1.getSpeed());
+
+            Car car2 = new Car();
+            car2.make = "Ford";
+            car2.model = "Focus";
+            car2.setTopSpeed(100);
+            for (int i = 0; i < 50; i++)
+            {
+                car2.SpeedUp();
+                car2.ToString();
+            }
+            */
+
+            Car car3 = new Car();
+            car3.make = "Ford";
+            car3.model = "Focus";
+            car3.setTopSpeed(220);
+            car3.ToString();
+            for (int i = 0; i < 55; i++)
+            {
+                car3.SpeedUp();
+            }
+            car3.ToString();
+            car3.Honk();
+            while (car3.getSpeed() > 25)
+            {
+                car3.SlowDown();
+            }
+            car3.ToString();
+            car3.Beep();
+
+
+        }
+    }
+}
